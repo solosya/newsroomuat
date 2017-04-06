ae28760cb262f45be0aaec81bee32850f0f4b15b
Alex Solosy <ASolosy@pagemasters.com.au>
2017-04-06 18:16:20 +1000

    sdk files

---
+++ assets/scripts/sdk/blog.js
@@ -0,0 +1,59 @@
+    $.fn.Ajax_LoadBlogArticles = function(options){
+        console.log('loading more blog articles mamma');
+        var defaults = {
+            'limit': 20,
+            'containerClass': 'ajaxArticles',
+            'onSuccess' : function(){},
+            'onError' : function(){},
+            'beforeSend' : function(){},
+            'onComplete' : function(){}
+        };
+        
+        var opts = $.extend( {}, defaults, options );
+        
+        var offset = parseInt($('.'+opts.containerClass).data('offset'));
+        if(isNaN(offset) || offset < 0) {
+            offset = opts.limit;
+        }
+        
+        var existingNonPinnedCount = parseInt($('.'+opts.containerClass).data('existing-nonpinned-count'));
+        if(isNaN(existingNonPinnedCount)) {
+            existingNonPinnedCount = -1;
+        }
+        
+        $('.'+opts.containerClass).data('offset', (offset + opts.limit));
+        
+        var csrfToken = $('meta[name="csrf-token"]').attr("content");
+        
+        var dateFormat = 'SHORT';
+        // console.log(_appJsConfig.baseHttpPath + '/home/load-articles');
+        _appJsConfig.baseHttpPath = 'http://theme.aap.io';
+        
+        $.ajax({
+            type: 'get',
+            url: _appJsConfig.baseHttpPath + '/home/load-articles',
+            dataType: 'json',
+            data: {offset: offset, limit: opts.limit, existingNonPinnedCount: existingNonPinnedCount, _csrf: csrfToken, dateFormat: dateFormat},
+            success: function (data, textStatus, jqXHR) {
+                if (opts.onSuccess && typeof opts.onSuccess === 'function') {
+                    opts.onSuccess(data, textStatus, jqXHR);
+                }                
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR.responseText);
+                if (opts.onError && typeof opts.onError === 'function') {
+                    opts.onError(jqXHR, textStatus, errorThrown);
+                }
+            },
+            beforeSend: function (jqXHR, settings) {
+                if (opts.beforeSend && typeof opts.beforeSend === 'function') {
+                    opts.beforeSend(jqXHR, settings);
+                }
+            },
+            complete: function (jqXHR, textStatus) {
+                if (opts.onComplete && typeof opts.onComplete === 'function') {
+                    opts.onComplete(jqXHR, textStatus);
+                }
+            }
+        });        
+    };
